b4d6b1df20c7916e48f728ef50df42d8f804f9de
Ashmita Khuntia <ashmitakhuntia10@gmail.com>
2026-08-07 20:28:20 +0530

    Added footer and Styles for home screen

---
+++ src/components/footer.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def footer_home():
+    logo_url = "https://i.ibb.co/YTYGn5qV/logo.png"
+    st.markdown(f"""
+        <div style='margin-top:0.5rem; display:flex; gap:6px; justify-content:center; item-align:center;'>
+            <p style='max-height:25px; font-weight:bold; color: white;'>Created with ❤️ by me</p>
+        </div>
+
+                """,unsafe_allow_html=True)
--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -6,6 +6,11 @@ def style_background_home():
             .stApp{
                 background-color: #5865F2 !important;
             }
+            .stApp div[data-testid="stColumn"]{
+                background-color: #E0E3FF !important;
+                padding: 2.7rem !important;
+                border-radius: 5rem !important;
+            }
         </style>    
             """,unsafe_allow_html=True)
 
@@ -23,6 +28,8 @@ def style_base_layout():
         <style>
         @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis:YEAR@1979&display=swap');
         @import url('https://fonts.googleapis.com/css2?family=Bitcount+Grid+Double:wght@100..900&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap');
+
 
            /* Hide Top Bar of streamlit */
              
@@ -41,10 +48,41 @@ def style_base_layout():
                 margin-bottom: 0rem !important;
             }
             h2 {
+                color: black !important;
                 font-family: 'Climate Crisis', sans-serif !important;
-                font-size: 3.5rem !important;
-                line-height: 1.1 !important;
+                font-size: 2rem !important;
+                line-height: 0.9 !important;
                 margin-bottom: 0rem !important;
+            }
+            h3, h4, p{
+                font-family: 'Outfit', sans-serif
+            }
+            button{
+                border-radius: 1.5rem !important;
+                background: #5865F2 !important;
+                color: white !important;
+                padding: 10px 20px !important;
+                border: none !important;
+                transition: transform 0.2ease-in-out !important;
+            }
+            button[kind="secondary"]{
+                border-radius: 1.5rem !important;
+                background: #EB459E !important;
+                color: white !important;
+                padding: 10px 20px !important;
+                border: none !important;
+                transition: transform 0.25s ease-in-out !important;
+            }
+            button[kind="tertiary"]{
+                border-radius: 1.5rem !important;
+                background: black !important;
+                color: white !important;
+                padding: 10px 20px !important;
+                border: none !important;
+                transition: transform 0.25s ease-in-out !important;
+            }
+            button:hover{
+                transform: scale(1.05)
             }
         </style>    
             """,unsafe_allow_html=True)
